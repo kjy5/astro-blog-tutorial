c7eccf574e87ae4edb507cc4c4429f3e02d10c14
Kenneth Yang <kjy5@uw.edu>
2025-01-15 21:45:23 -0800

    Dynamic tags page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kjy5.github.io",
   base: "astro-blog-tutorial",
+  integrations: [preact()],
 });
